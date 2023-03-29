95e8c9ffa5984486c6e40dde7b592d2f9789d75a
Ward Nakchbandi <hotrod.master@hotmail.com>
2023-03-29 18:56:37 +0300

    first commit

---
+++ main.sh
@@ -0,0 +1,20 @@
+# Add dependent repositories
+wget -q -O - https://ppa.pika-os.com/key.gpg | sudo apt-key add -
+add-apt-repository https://ppa.pika-os.com
+add-apt-repository ppa:pikaos/pika
+add-apt-repository ppa:kubuntu-ppa/backports
+# Clone Upstream
+https://github.com/v1cont/yad
+cp -rvf ./debian ./yad
+cd ./yad
+
+# Get build deps
+apt-get build-dep ./ -y
+
+# Build package
+dpkg-buildpackage
+
+# Move the debs to output
+cd ../
+mkdir -p ./output
+mv ./*.deb ./output/
