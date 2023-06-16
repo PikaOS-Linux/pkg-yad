a6f4a196862cf2d60f27c7a53319d32cae009f66
ferrreo <harderthanfire@gmail.com>
2023-06-16 19:23:14 +0100

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,8 +1,3 @@
-# Add dependent repositories
-wget -q -O - https://ppa.pika-os.com/key.gpg | sudo apt-key add -
-add-apt-repository https://ppa.pika-os.com
-add-apt-repository ppa:pikaos/pika
-add-apt-repository ppa:kubuntu-ppa/backports
 # Clone Upstream
 git clone https://github.com/v1cont/yad -b v12.3
 cp -rvf ./debian ./yad
